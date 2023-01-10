99fc7e4279d4eb6e89646b87e906bbc76c6022e0
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-01-10 12:00:31 +0530

    // Project Problem for student Rest API- Railway-GetPost

---
+++ 1.RailwayAndMeal_GETPOST/src/main/java/railway/com/example/RailwayAndMeal/service/RailwayService.java
@@ -0,0 +1,31 @@
+package railway.com.example.RailwayAndMeal.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import railway.com.example.RailwayAndMeal.Entity.Ticket;
+
+public class RailwayService {
+	
+	
+	public List<Ticket> list = new ArrayList<>();
+	public Map<Long,Ticket> ticketMap = new HashMap<>();
+	
+	//Complete the method body to add a ticket.
+	public void addTicket(Ticket ticket) {
+			
+	}
+	
+	//Complete the method body to return all tickets using list.
+	public List<Ticket> getAllTickets() {		
+		
+	}
+	
+	//Complete the method body to return a single ticket using PNR.
+	public Ticket getTicketByPnr(long pnr) {
+		
+	}
+		
+}
