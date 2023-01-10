10b344addd582371da5b318dbbc3c0bcafb7a06f
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-01-10 12:43:13 +0530

    // Project Student

---
+++ 2.RailwayAndMeal_UPDATEDELETE/src/main/java/railway/com/example/RailwayAndMeal/controller/controller.java
@@ -0,0 +1,60 @@
+package railway.com.example.RailwayAndMeal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import railway.com.example.RailwayAndMeal.Entity.Ticket;
+import railway.com.example.RailwayAndMeal.service.RailwayService;
+
+@RestController
+@RequestMapping("/railway")
+public class controller {
+
+	@Autowired
+	private RailwayService railwayservice;		
+	
+	
+	@PostMapping("/ticket")
+	public void addTicket(@RequestBody Ticket ticket) {
+		railwayservice.addTicket(ticket);
+	}
+	
+	
+	@GetMapping("/tickets")
+	public List<Ticket> getAllTickets(){
+		return railwayservice.getAllTickets();
+	}
+	
+	@GetMapping("/ticket/{pnr}")
+	public Ticket getTicketByPnr(@PathVariable long pnr) {
+		return railwayservice.getTicketByPnr(pnr);
+	}
+	
+	/*
+	 1. Call the required service method
+	 2. Add proper annotations for Delete Mapping and fetch the
+	 	PNR from PathVariable.
+	 */
+	public Ticket deleteTicket(long pnr) {
+		
+	}
+	
+	/*
+	 1. Call the required service method
+	 2. Add proper annotations for Update Mapping and use RequestBody
+	 annotation to bind the request body to ticket.
+	 */
+	public Ticket updateTicket(Ticket ticket) {
+		
+	}
+
+}
