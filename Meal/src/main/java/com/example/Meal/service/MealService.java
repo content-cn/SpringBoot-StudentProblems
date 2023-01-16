5b502c1967aa275aefa358dc11692166d402d829
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-01-16 13:41:55 +0530

    // Student Problem Meal Service _ the Meal Service  created by Ashustosh

---
+++ Meal/src/main/java/com/example/Meal/service/MealService.java
@@ -0,0 +1,73 @@
+package com.example.Meal.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import com.example.Meal.Entity.Meal;
+import com.example.Meal.exception.MealNotFoundException;
+import com.example.Meal.exception.MealWithPnrAlreadyExists;
+
+@Service
+public class MealService {
+	
+	public List<Meal> list = new ArrayList<>();
+	
+	public List<Meal> getMeals() {
+		return list;
+	}	
+	
+	public Meal getMealByPnr(long pnr) {
+		if(ObjectUtils.isEmpty(getMealfromList(pnr))) {
+			throw new MealNotFoundException("meal with given pnr " + pnr + " does not exist.");
+		}
+		return getMealfromList(pnr);
+	}
+	
+	public void addMeal(Meal meal) {
+		long pnr = meal.getPnr();
+		
+		if(!ObjectUtils.isEmpty(getMealfromList(pnr)))
+			deleteMeal(meal.getPnr());
+			
+		list.add(meal);
+	}
+	
+	public void deleteMeal(long pnr) {
+		
+		if(ObjectUtils.isEmpty(getMealfromList(pnr))) {
+			System.out.println("Meal with given Pnr does not exist");
+			throw new MealNotFoundException("Meal with given Pnr does not exist");
+		}
+		
+		Meal meal = this.getMealfromList(pnr);
+		list.remove(meal);
+	}
+	
+	
+	public void updateMeal(Meal meal, long pnr) {
+		if(ObjectUtils.isEmpty(getMealfromList(pnr))) {
+			System.out.println("Meal with given Pnr does not exist");
+			throw new MealNotFoundException("Meal with given Pnr does not exist");
+		}
+		
+		this.deleteMeal(pnr);
+		this.addMeal(meal);
+		
+	}
+	
+	public Meal getMealfromList(Long pnr) {
+		for(Meal meal: list) {
+			if(meal.getPnr() == pnr)
+				return meal;
+		}
+		
+		return null;
+	}
+}
