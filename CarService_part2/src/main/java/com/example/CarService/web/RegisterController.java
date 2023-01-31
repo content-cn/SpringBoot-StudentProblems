6f12e6eee9f287fd7eccde87685a2b5e94a4a2d2
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-01-31 13:03:19 +0530

    MVC core Principles Student Problem Car Service 2

---
+++ CarService_part2/src/main/java/com/example/CarService/web/RegisterController.java
@@ -0,0 +1,35 @@
+package com.example.CarService.web;
+
+import com.example.CarService.domain.Car;
+import com.example.CarService.domain.Vehicle;
+import com.example.CarService.service.CarRegistrationService;
+import com.example.CarService.service.Registration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RegisterController {
+    @Autowired
+    Registration registration;
+
+    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    public String getRegistrationPage(Model carModel){
+        Vehicle vehicle=registration.getNewCar();
+        carModel.addAttribute("Vehicle",vehicle);
+        return "carregister";
+    }
+    @RequestMapping(value = "/done")
+    public String getResponsePage(@ModelAttribute("vehicle") Car car){
+        if(registration.registerCar(car.getRegisterationNumber(),car.getCarName(),car.getCarDetails(), car.getCarWork())){
+            return "success";
+        }else{
+            return "carregister";
+        }
+
+    }
+
+}
