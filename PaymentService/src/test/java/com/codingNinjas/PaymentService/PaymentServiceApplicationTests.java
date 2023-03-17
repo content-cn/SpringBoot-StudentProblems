5cafff0990e7d3b77c10e2e5d4ed5f5181b2dd65
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-03-17 15:19:00 +0530

    Lecture 1 Introduction to Spring Boot  Payment Service Problem

--- PaymentService/src/test/java/com/codingNinjas/PaymentService/PaymentServiceApplicationTests.java
+++ PaymentService/src/test/java/com/codingNinjas/PaymentService/PaymentServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.CustomerServicedemo;
+package com.codingNinjas.PaymentService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerServicedemoApplicationTests {
+class PaymentServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
