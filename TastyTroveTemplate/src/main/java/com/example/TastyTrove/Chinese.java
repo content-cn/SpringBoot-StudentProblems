e78a285b0d2c5b330cdcf19cf472f4057d2bc734
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-04-17 13:56:41 +0530

    Lecture 2 Dependency Student Problem : Recipe

---
+++ TastyTroveTemplate/src/main/java/com/example/TastyTrove/Chinese.java
@@ -0,0 +1,13 @@
+package com.example.TastyTrove;
+
+/*
+ * 1. Add class attributes.
+ * 2. Override all the interface methods.
+ * 3. Implement setter injection
+ */
+
+public class Chinese implements Recipe {
+
+
+
+}
---
+++ TastyTroveTemplate/src/main/java/com/example/TastyTrove/SouthIndian.java
@@ -0,0 +1,11 @@
+package com.example.TastyTrove;
+
+/*
+ * 1. Add class attributes.
+ * 2. Override all the interface methods.
+ * 3. Implement setter injection
+ */
+
+public class SouthIndian implements Recipe {
+
+}
