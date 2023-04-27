bc3e07f6fae61b66a02a97dacd834e503bc9079a
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-04-27 12:22:30 +0530

    Lecture Bean Scope  Student Problem Bank registration 2

---
+++ Bank-Account-Registration-II-Template/src/main/java/com/codingNinjas/Bank/Account/Registration/myUser.java
@@ -0,0 +1,12 @@
+package com.codingNinjas.Bank.Account.Registration;
+
+/*
+ * 1. Add class attributes.
+ * 2. Override all the interface methods.
+ * 3. Implement setter injection
+ * 4. Add custom init and destroy methods
+ */
+
+public class myUser implements User{
+
+}
